940c618b77f60123574a81a1298697d3440441e3
Chriskao1 <159023811+Chriskao1@users.noreply.github.com>
2024-02-06 20:35:08 +0800

    Lecture 2-1
    請完成拋體運動的模擬，使球碰到地板會發生反彈，並使用指令描繪出球的運動軌跡。接著請再新增一個球並加入空氣阻力的影響，保留沒有空氣阻力拋體的軌跡與之對照，空氣阻力常數 k 可以自行設定。

---
+++ kaoChris_Lecture2-1.py
@@ -0,0 +1,37 @@
+from vpython import *
+#Web VPython 3.2
+
+size=1.5
+scene = canvas(width=600, height=600, center = vector(0,5,0)) #設定畫面
+floor = box(length=150, height=0.01, width=50, color=color.green)      #畫地板
+bally = sphere(radius = size, color=color.yellow, make_trail= True, trail_type="points", interval=100)
+ballr = sphere(radius = size, color=color.red, make_trail=True, trail_type="points", interval=100)
+
+bally.pos = vector(-70, 0, 0)#球初始位置
+ballr.pos = vector(-70, 0, 0)#球初始位置
+bally.v = vector(10, 10, 0) #球初速 
+ballr.v = vector(10, 10, 0) #球初速 
+g=9.8
+m=10
+k=0.05
+
+dt = 0.001	#時間間隔 0.001 秒
+t = 0.0		#模擬初始時間為0秒
+
+while bally.v.y != 0:    #模擬直到球落地 即y=球半徑
+    rate(1/dt)    #每一秒跑 1000 次
+    t = t + dt    #計時器
+    bally.v.y -= g * dt       #球的末速度 = 前一刻速度 + 加速度*時間間隔
+    bally.pos = bally.pos + bally.v * dt#球的末位置 = 前一刻位置 + 速度*時間間隔
+    air_resist = -k * ballr.v
+    if ballr.v.y > 0:
+        ballr.v.y -= g * dt
+        ballr.v += air_resist * dt
+    else:
+        ballr.v.y -= g * dt
+        ballr.v += air_resist * dt
+    ballr.pos += ballr.v * dt
+    if bally.v.y < 0 and bally.pos.y <= size:
+        bally.v.y = -bally.v.y
+    if ballr.v.y < 0 and ballr.pos.y <= size:
+        ballr.v.y = -ballr.v.y
